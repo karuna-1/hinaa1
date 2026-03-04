f2e32007d6a92e94188c3f7240020a88c5e2b267
karuna-1 <161311292+karuna-1@users.noreply.github.com>
2026-03-04 10:18:13 +0530

    file added

---
+++ InputExample.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class InputExample {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter username:");
+
+        String userName = myObj.nextLine(); // Reads user input
+        System.out.println("Username is: " + userName);
+    }
+}
